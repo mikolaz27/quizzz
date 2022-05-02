fad8d34d24658aec53c8fb5dec2d260c917c4ee7
Михайло Лазорик <mikolaz2727@gmail.com>
2022-05-02 21:34:42 +0300

    Added tests and models

--- src/quiz/models.py
+++ src/quiz/models.py
@@ -1,3 +1,48 @@
+import uuid
+
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    QUESTION_MAX_LIMIT = 20
+    QUESTION_MIN_LIMIT = 3
+
+    class LEVEL_CHOICES(models.IntegerChoices):
+        BASIC = 0, "Basic"
+        MIDDLE = 1, "Middle"
+        ADVANCED = 2, "Advanced"
+
+    category = models.ForeignKey(to="quiz.Category", related_name="quizzes", on_delete=models.CASCADE)
+    uuid = models.UUIDField(default=uuid.uuid4, db_index=True, unique=True)
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=1024, null=True, blank=True)
+    level = models.PositiveSmallIntegerField(choices=LEVEL_CHOICES.choices, default=LEVEL_CHOICES.BASIC)
+    image = models.ImageField(default="default.png", upload_to="media/covers")
+
+    def questions_count(self):
+        return self.questions.count()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+class Result(models.Model):
+    user = models.ForeignKey(to=get_user_model(), related_name="results", on_delete=models.CASCADE)
+    quiz = models.ForeignKey(to="quiz.Quiz", related_name="results", on_delete=models.CASCADE)
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(to="quiz.Quiz", related_name="questions", on_delete=models.CASCADE)
+    order_number = models.PositiveSmallIntegerField(validators=[MaxValueValidator(Quiz.QUESTION_MAX_LIMIT)])
+    text = models.CharField(max_length=512)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(to="quiz.Question", related_name="choices", on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    pass
--- src/quiz/tests/tests.py
+++ src/quiz/tests/tests.py
@@ -1,6 +1,34 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
+from quiz.models import Category, Question, Quiz
 
-class TestMyFirstTest(TestCase):
-    def test_my_first_test(self):
-        assert 1 == 1
+
+def sample_quiz(title, **params):
+    defaults = {"description": "Some text", "category": Category.objects.create()}
+    defaults.update(params)
+    return Quiz.objects.create(title=title, **defaults)
+
+
+def sample_question(quiz, order_number, **params):
+    defaults = {"text": "Some text"}
+    defaults.update(params)
+    return Question.objects.create(quiz=quiz, order_number=order_number, **defaults)
+
+
+class TestQuizModel(TestCase):
+    def setUp(self) -> None:
+        self.questions_count = 10
+        self.test_quiz = sample_quiz(title="test_quiz")
+        for i in range(self.questions_count):
+            sample_question(quiz=self.test_quiz, order_number=i)
+
+    def tearDown(self) -> None:
+        self.test_quiz.delete()
+
+    def test_questions_count_normal_case(self):
+        self.assertEqual(self.questions_count, self.test_quiz.questions_count())
+
+    def test_title_limit(self):
+        with self.assertRaises(ValidationError):
+            sample_quiz(title="i" * 70000)
