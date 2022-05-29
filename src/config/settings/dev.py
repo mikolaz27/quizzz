3a87403f9a73a7039974868e984652520f6413f9
mikolaz27 <mikolaz27@outlook.com>
2022-05-29 23:49:41 +0300

    Check CI/CD

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,6 +1,21 @@
+import os
+
 from config.settings.base import *  # noqa:
 
 DEBUG = True
 
 CURRENT_ENV = "DEV"
 print(CURRENT_ENV)
+
+
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
